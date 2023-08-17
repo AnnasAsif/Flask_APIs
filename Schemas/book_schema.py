d391fc2f89504bb7fea172bb95f787f036872919
Muhammad Annas Asif <108392832+AnnasAsif@users.noreply.github.com>
2023-08-17 15:29:02 +0500

    no need of schemas in this project till now

--- Schemas/book_schema.py
+++
@@ -1,9 +0,0 @@
-# book_schema.py
-from mongoengine import Document, StringField, DateField, IntField
-
-class Book(Document):
-    title = StringField(required=True)
-    author = StringField(required=True)
-    publication_date = DateField(default=Date.now)
-    pages = IntField(required=True)
-    genre = StringField(required=True)
